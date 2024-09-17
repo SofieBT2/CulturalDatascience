1269ab9254cc86b859b8741d5beb25db32a3c2b0
SofieBT2 <sofie_bt@hotmail.com>
2024-09-17 12:20:19 +0200

    Added new file

---
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -0,0 +1,9 @@
+!/usr/bin/env bash
+
+# any python packages you want to have preinstalled
+pip install pingouin
+pip install lxml
+
+# your git config information
+git config --global user.email "sofie_bt@hotmail.com"
+git config --global user.name "SofieBT2"
